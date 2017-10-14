a77ab36c299b2fb5f76ded96dd434a7f9fb39ba0
Oskar Weser <oskar.weser@gmail>
2017-10-14 10:31:01 +0200

    ENH: create output

--- src/chemopt/zmat_optimisation.py
+++ src/chemopt/zmat_optimisation.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 from scipy.optimize import minimize
 
@@ -10,7 +11,7 @@ def convert(x):
     return convertor(x, 'hartree', 'eV') / convertor(1, 'bohr', 'Angstrom')
 
 
-def optimise(zmolecule, symbols=None, **kwargs):
+def optimise(zmolecule, output, symbols=None, **kwargs):
     """Optimize a molecule.
 
     Args:
@@ -26,7 +27,8 @@ def optimise(zmolecule, symbols=None, **kwargs):
     Returns:
         :class:`chemcoord.Cartesian`: A new cartesian instance.
     """
-    V = _create_V_function(zmolecule, **kwargs)
+    V = _create_V_function(zmolecule, output, **kwargs)
+    create_output(output, zmolecule, **kwargs)
     opt = minimize(V, x0=_extract_C_rad(zmolecule), jac=True, method='BFGS')
     return opt
 
@@ -37,14 +39,13 @@ def _extract_C_rad(zmolecule):
     return C_rad.flatten(order='F')
 
 
-def _create_V_function(zmolecule, **kwargs):
+def _create_V_function(zmolecule, outputfile, **kwargs):
     get_zm_from_C = _get_zm_from_C_generator(zmolecule)
 
-    def V(C_rad):
+    def V(C_rad, calculated=[], get_calculated=False):
         zmolecule = get_zm_from_C(C_rad)
-        molecule = zmolecule.get_cartesian()
 
-        result = calculate(molecule=molecule, forces=True, **kwargs)
+        result = calculate(molecule=zmolecule, forces=True, **kwargs)
         energy = result.scfenergies[0]
         grad_energy_X = convert(result.grads[0])
 
@@ -57,7 +58,11 @@ def _create_V_function(zmolecule, **kwargs):
             grad_energy_C[i, i:] = 0
 
         grad_energy_C = grad_energy_C.flatten()
-        return energy, grad_energy_C
+        calculated.append((zmolecule, energy, grad_energy_C))
+        if get_calculated:
+            return calculated
+        else:
+            return energy, grad_energy_C
     return V
 
 
@@ -66,8 +71,34 @@ def _get_zm_from_C_generator(zmolecule):
         C_deg = C_rad.copy().reshape((3, len(C_rad) // 3), order='F').T
         C_deg[:, [1, 2]] = np.rad2deg(C_deg[:, [1, 2]])
 
-        new_zm = previous_zm.pop().copy()
+        new_zm = previous_zm[-1].copy()
         new_zm.safe_loc[zmolecule.index, ['bond', 'angle', 'dihedral']] = C_deg
         previous_zm.append(new_zm)
         return new_zm
     return get_zm_from_C
+
+
+def create_ouput(output, zmolecule, **kwargs):
+    header = """\
+This is ChemOpt {version} ptimising a molecule in internal coordinates.
+
+Structure as Zmatrix:
+{zmat}
+Structure in cartesian coordinates:
+{cartesian}
+
+The setup for the electronic calculation is:
+Backend: {backend}
+Theory: {theory}
+Basis: {basis}
+Charge: {charge}
+Multiplicity: {multiplicity}
+
+Starting at {time}:
+n energy Δ
+""".format(version=chemopt.__version__, zmat=zmolecule.to_zmat(),
+           cartesian=zmolecule.get_cartesian().to_xyz(),
+           time=time.asctime(), **kwargs)
+
+    with open(output, 'x') as f:
+        f.write(header)
